9f73575423942010b31eb4a7eeacc4f4bc22dee1
Sun Zhenbao <sunzhenbao@nationsky.com>
2018-08-09 14:29:22 +0800

    catch exceptioin.

--- imgstorage/ImgStorage.cpp
+++ imgstorage/ImgStorage.cpp
@@ -26,16 +26,22 @@ ImgStorage::~ImgStorage()
 
 int ImgStorage::UploadFile(const std::string& filename,std::string& remoteurl)
 {
-	std::string ip = Config::GetInstance()->GetData().storage.ip;
-	int port = Config::GetInstance()->GetData().storage.port;
-	fastdfs_sdk sdk;
-	int code = sdk.connect(ip.c_str(), port);
-	ST_RETURN_DATA data = sdk.upload_file(filename.c_str(), true);
-	char url[256];
-	memset(url, 0, 256);
-	code = sdk.remote_file_to_ulr(data, url);
-	remoteurl = url;
-	return code;
+	try
+	{
+		std::string ip = Config::GetInstance()->GetData().storage.ip;
+		int port = Config::GetInstance()->GetData().storage.port;
+		fastdfs_sdk sdk;
+		int code = sdk.connect(ip.c_str(), port);
+		ST_RETURN_DATA data = sdk.upload_file(filename.c_str(), true);
+		char url[256];
+		memset(url, 0, 256);
+		code = sdk.remote_file_to_ulr(data, url);
+		remoteurl = url;
+		return code;
+	}
+	catch (...) {
+		return -1;
+	}
 }
 
 
